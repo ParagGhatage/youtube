6709f7492aba8f8112f140c7499af773c25f0235
ParagGhatage <phghatage1@gmail.com>
2024-03-17 14:42:54 +0530

    Sign Up page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,136 +1,106 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import React,{useState} from 'react'
-import { Link,NavLink } from 'react-router-dom'
-import axios from 'axios'
+function Signup() {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        userName: '',
+        email: '',
+        password: '',
+        avatar: null,
+    });
 
-function Signup(){
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
-    
-        // Define state variables to store form data
-        const [formData, setFormData] = useState({
-            fullName:'',
-          userName: '',
-          email: '',
-          password: '',
-          avatar:null,
-
-        });
-      
-        // Function to handle form input changes
-        const handleInputChange = (event) => {
-          const { name, value } = event.target;
-          setFormData({
-            ...formData,
-            [name]: value
-          });
-        };
-        const handleFileChange = (event) => {
-            setFormData({
-              ...formData,
-              avatar: event.target.files[0] // Store the selected file
-            });
-          };
-      
-        // Function to handle form submission
-        const handleSubmit = async (event) => {
-            event.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
             const formDataToSend = new FormData();
-            formDataToSend.append('fullname', formData.fullName);
-            formDataToSend.append('username', formData.userName);
+            formDataToSend.append('fullName', formData.fullName);
+            formDataToSend.append('userName', formData.userName);
             formDataToSend.append('email', formData.email);
             formDataToSend.append('password', formData.password);
-            formDataToSend.append('file', formData.avatar); // Append the file to FormData
-        
-            // Process the form data here (e.g., send it to a server)
-            console.log(formDataToSend);
+            formDataToSend.append('avatar', formData.avatar);
 
-        const response = await axios.post('http://localhost:8000/api/v1/users/register', formDataToSend, {
-        headers: {
-          'Content-Type': 'multipart/form-data' // Set content type for FormData
+            const response = await axios.post('http://localhost:8000/api/v1/users/register', formDataToSend);
+            console.log('Response:', response.data);
+        } catch (error) {
+            console.error(error);
         }
-      });
-      console.log('Response:', response.data);
-    
-            setFormData({
-            fullName:'',
-          userName: '',
-          email: '',
-          password: '',
-          avatar:null,
+    };
 
-              });
-            };
+    const handleFile = (e) => {
+        if (e.target.name === 'avatar') {
+            setFormData({ ...formData, avatar: e.target.files[0] });
+        } else {
+            setFormData({ ...formData, [e.target.name]: e.target.value });
+        }
+    };
 
     return (
-        
-        
         <div className='text-black'>
             Sign Up
-
-            <form onSubmit={handleSubmit}>
-            <label>
-        Fullname:
-        <input
-          type="text"
-          name="fullName"
-          value={formData.fullName}
-          onChange={handleInputChange}
-          className=' p-3 rounded-sm m-4 border-black'
-        />
-      </label>
-      <br />
-      <label>
-        Username:
-        <input
-          type="text"
-          name="userName"
-          value={formData.userName}
-          onChange={handleInputChange}
-          className=' p-3 rounded-sm m-4'
-        />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          className=' p-3 rounded-sm m-4'
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          className=' p-3 rounded-sm m-4'
-        />
-      </label>
-      <br />
-      
-      <label>
-        Choose Avatar:
-        <input
-          type="file"
-          name="avatar"
-          onChange={handleFileChange}
-          className=' p-3 rounded-sm m-4'
-        />
-      </label>
-      <br />
-      <button type="submit" className='bg-green-500 p-3 rounded-sm m-4'>Submit</button>
-    </form>
-
-
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+                <label>
+                    Fullname:
+                    <input
+                        type="text"
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                        className='p-3 rounded-sm m-4 bg-red-300 border-black'
+                    />
+                </label>
+                <br />
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        name="userName"
+                        value={formData.userName}
+                        onChange={handleChange}
+                        className='p-3 rounded-sm bg-red-300 m-4'
+                    />
+                </label>
+                <br />
+                <label>
+                    Email:
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        className='p-3 rounded-sm bg-red-300 m-4'
+                    />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        className='p-3 rounded-sm bg-red-300 m-4'
+                    />
+                </label>
+                <br />
+                <label>
+                    Choose Avatar:
+                    <input
+                        type="file"
+                        name="avatar"
+                        onChange={handleFile}
+                        className='p-3 rounded-sm bg-red-300 m-4'
+                    />
+                </label>
+                <br />
+                <button type="submit" className='bg-green-500 p-3 rounded-sm m-4'>Submit</button>
+            </form>
         </div>
-        
-    )
-
+    );
 }
 
 export default Signup;
